9514a7206706d53a7a4406796225d5efacb4d99b
Samuel Koshy <67376389+Samuel-Koshy2000@users.noreply.github.com>
2024-10-20 02:43:31 +0200

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.womenshopfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.womenshopfx to javafx.fxml;
-    exports org.example.womenshopfx;
-}
